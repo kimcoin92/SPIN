19ab87f8837823187103ffead0619ad6b0ff3e59
Kimcoin <kimcoin92@gmail.com>
2022-05-27 23:17:28 +0900

    initial commit

---
+++ data/script/main.js
@@ -0,0 +1,472 @@
+// 즉시 호출 함수
+(() => {
+
+
+
+    //  ########################################################
+    //  #                     초기화 세팅                      #
+    //  ########################################################
+
+
+    
+    let yOffset                     = 0;
+    let sectionYOffset              = 0;
+    let currentSection              = 0;
+
+    let introFadeOutTimer           = null;
+    let introTransTimer             = null;
+
+    let introTransLength            = 0;
+    let introTranslateValue1        = 0;
+    let introTranslateValue2        = 0;
+
+    let introOpacityValue           = 0;
+
+
+
+    //  ########################################################
+    //  #                       세팅 값                        #
+    //  ########################################################
+
+
+    
+    // 메뉴 섹션
+
+    const menuLogo           = document.querySelector('.global-nav-links #global-logo');
+    const menuLink1          = document.querySelector('.global-nav-links #global-nav-1');
+    const menuLink2          = document.querySelector('.global-nav-links #global-nav-2');
+    const menuLink3          = document.querySelector('.global-nav-links #global-nav-3');
+
+    let sectionSet = [
+
+        // 인트로 섹션
+        {
+
+            height           : 0,
+            heightMultiply   : 1.5,
+
+            objects: {
+
+                container          : document.querySelector('#section-0'),
+                introElement1_text : document.querySelector('#section-0 #box1'),
+                introElement2_text : document.querySelector('#section-0 #box2'),
+                videoElement       : document.querySelector('.background-video')
+
+            }
+
+        },
+
+        // 번호 조회 섹션
+        {
+
+            height         : 0,
+            heightMultiply : 1.5,
+
+            objects: {
+
+                container           : document.querySelector('#section-1'),
+
+                resultElement1_text : document.querySelector('#section-1 #text1'),
+                resultElement2_text : document.querySelector('#section-1 #text2'),
+                resultElement3_box  : document.querySelector('#section-1 #box1'),
+
+            },
+
+            values: {
+
+                resultElement1_OpacityOut    : [0, 1, {start: 0.05, end: 0.15}],
+                resultElement1_TranslateOut  : [0, -10, {start: 0.05, end: 0.15}],
+ 
+            }
+
+        },
+
+        // 번호 생성 섹션
+        {
+
+            height         : 0,
+            heightMultiply : 1.5,
+
+            objects: {
+
+                container               : document.querySelector('#section-2'),
+                generateElement1_text   : document.querySelector('#section-2 #text1'),
+                generateElement2_text   : document.querySelector('#section-2 #text2'),
+                generateElement3_button : document.querySelector('#section-2 #button-Generate'),
+                generateElement4_list   : document.querySelector('#section-2 #number-List')
+
+            },
+
+            values: {
+
+                generateElement1_OpacityOut    : [0, 1, {start: 0.1, end: 0.15}],
+                generateElement1_TranslateOut  : [0, -10, {start: 0.1, end: 0.15}],
+                generateElement2_OpacityOut    : [0, 1, {start: 0.1, end: 0.2}],
+                generateElement2_TranslateOut  : [0, -10, {start: 0.1, end: 0.2}],
+                generateElement3_OpacityOut    : [0, 1, {start: 0.2, end: 0.25}],
+                generateElement3_TranslateOut  : [0, -10, {start: 0.2, end: 0.25}],
+                generateElement4_OpacityOut    : [0, 1, {start: 0.25, end: 0.2}],
+                generateElement4_TranslateOut  : [0, -10, {start: 0.25, end: 0.2}],
+ 
+            }
+
+        },
+
+        // 문의 섹션
+        {
+
+            height         : 0,
+            heightMultiply : 1.5,
+
+            objects: {
+
+                container  : document.querySelector('#section-3')
+
+            },
+
+            values: {
+
+                generateElement1_OpacityOut    : [0, 1, {start: 0.1, end: 0.2}],
+                generateElement1_TranslateOut  : [0, -10, {start: 0.1, end: 0.2}],
+ 
+            }
+
+        }
+
+    ];
+
+
+
+    //  ########################################################
+    //  #                        함 수                         #
+    //  ########################################################
+
+
+
+    // 페이지 시작 호출
+    const initHTMLPage = function() {
+
+        initSectionSet();
+        getCurrentSection();
+
+        playIntro();
+
+    }
+
+    // 섹션 별 페이지 길이 보정
+    const initSectionSet = function() {
+
+        for (let i = 0; i < sectionSet.length; i++) {
+
+            sectionSet[i].height = window.innerHeight * sectionSet[i].heightMultiply; 
+            sectionSet[i].objects.container.style.height = `${sectionSet[i].height}px`;
+
+        }
+
+    }
+
+    // 현재 화면 상 표시된 섹션을 도출
+    const getCurrentSection = function() {
+
+        let result = 0;
+
+        if (yOffset <= sectionSet[0].height) {
+
+            result = 0;
+
+        } else if ((yOffset > sectionSet[0].height) && (yOffset <= sectionSet[0].height + sectionSet[1].height)) {
+
+            result = 1;
+
+        } else if ((yOffset > sectionSet[0].height + sectionSet[1].height) && (yOffset <= sectionSet[0].height + sectionSet[1].height + sectionSet[2].height)) {
+
+            result = 2;
+
+        } else if (yOffset > sectionSet[0].height + sectionSet[1].height + sectionSet[2].height) {
+
+            result = 3;
+
+        }
+
+        return result;
+        
+    }
+
+    // 이전 세션의 높이 도출
+    const getPrevSectionHeight = function() {
+
+        let result = 0;
+
+        for (let i = 0; i < currentSection; i++) {
+
+            result = result + sectionSet[i].height;
+
+        }
+
+        return result;
+
+    };
+
+    // 반응형 애니메이션을 위한 범위 계산
+    const calcValue = function(values) {
+        
+        let result         = 0;
+        let rate           = 0;
+        
+        let partStart      = 0;
+        let partEnd        = 0;
+        let partHeight     = 0;
+
+        const range = values[1] - values[0];
+        const sectionHeight = sectionSet[currentSection].height;
+
+        if (values.length === 3) {
+
+            partStart = sectionHeight * values[2].start;
+            partEnd = sectionHeight * values[2].end;
+            partHeight = partEnd - partStart;
+
+            if ((sectionYOffset >= partStart) && (sectionYOffset <= partEnd)) {
+
+                rate = (sectionYOffset - partStart) / partHeight;
+                result = (rate * range) + values[0];               
+
+            } else if (sectionYOffset < partStart) {
+
+                result = values[0];
+
+            } else if (sectionYOffset > partEnd) {
+
+                result = values[1];
+
+            }
+            
+        } else {
+            
+            rate = sectionYOffset / sectionHeight;
+            result = (range * rate) + values[0];
+      
+        }
+
+        return result;
+
+    }
+
+    // 번호 무작위 생성
+    const getLottoNumber = function() {
+
+        let ball      = [];
+        let result    = [];
+        let remainIdx = 44;
+
+        for (let i = 0; i < 45; i++) {
+
+            ball[i] = (i + 1);
+
+        }
+
+        for (let k = 0; k < 6; k++) {
+
+            idx = Math.floor(Math.random() * remainIdx)
+
+            result.push(ball[idx]);
+
+            ball.splice(idx, 1);
+            remainIdx--;
+
+        };
+
+        result.sort(function (a, b) {
+
+            return a - b;
+
+        });
+
+        return result;
+
+    };
+
+    // 인트로 텍스트 등장 효과
+    const playIntroOpacity = function() {
+
+        if (introOpacityValue < 1) {
+
+            sectionSet[0].objects.introElement1_text.style.opacity = `${introOpacityValue}`;
+            sectionSet[0].objects.introElement2_text.style.opacity = `${introOpacityValue}`;
+            sectionSet[0].objects.videoElement.style.opacity       = `${introOpacityValue}`;
+
+            introOpacityValue += 0.025;
+
+        } else {
+
+            introOpacityValue = 1;
+
+            sectionSet[0].objects.introElement1_text.style.opacity = `${introOpacityValue}`;
+            sectionSet[0].objects.introElement2_text.style.opacity = `${introOpacityValue}`;
+            sectionSet[0].objects.videoElement.style.opacity       = `${introOpacityValue}`;
+
+            clearInterval(introFadeOutTimer);
+
+            introFadeOutTimer = null;
+
+            introOpacityValue = 0;
+
+            return;
+
+        };
+
+    };
+
+    // 인트로 텍스트 이동 효과
+    const playIntroTranslate = function() {
+
+        if (introTransLength < 1) {
+
+            sectionSet[0].objects.introElement1_text.style.transform = `translateX(${introTranslateValue1}px)`
+            sectionSet[0].objects.introElement2_text.style.transform = `translateX(${introTranslateValue2}px)`
+
+            introTransLength += 0.025;
+
+            introTranslateValue1 -= 0.5;
+            introTranslateValue2 += 0.5;
+
+        } else {
+
+            sectionSet[0].objects.introElement1_text.style.transform = `translateX(${introTranslateValue1}px)`
+            sectionSet[0].objects.introElement2_text.style.transform = `translateX(${introTranslateValue2}px)`
+
+            clearInterval(introTransTimer);
+
+            introTransTimer = null;
+
+            introTransLength = 0;
+            introTranslateValue1 = 0;
+            introTranslateValue2 = 0;
+
+            return;
+
+        };
+
+    };
+
+    // 인트로 애니메이션 트리거
+    const playIntro = function() {
+
+        introFadeOutTimer                = setInterval(playIntroOpacity, 20);
+        introTransTimer                  = setInterval(playIntroTranslate, 20);
+
+    };
+
+    // 스크롤 반응 애니메이션
+    const playScroll = function() {
+
+        let opacityValue = 0;
+        let translateValue = 0;
+
+        let that = sectionSet[currentSection];
+
+        const offsetRate = sectionYOffset / that.height;
+
+        switch(currentSection)
+        {
+            case 0 :
+                
+            break;
+
+            case 1 :
+
+                if (offsetRate > 0.05 && offsetRate < 0.15) {
+
+                    opacityValue = calcValue(that.values.resultElement1_OpacityOut);
+                    translateValue = calcValue(that.values.resultElement1_TranslateOut);
+
+                    that.objects.resultElement3_box.style.opacity = `${opacityValue}`;
+                    that.objects.resultElement3_box.style.transform = `translateY(${translateValue}px)`;
+
+                } else if (offsetRate > 0.15) {
+
+                    that.objects.resultElement3_box.style.opacity = `1`;
+
+                } else {
+
+                    that.objects.resultElement3_box.style.opacity = `0`;
+
+                };
+                
+            break;
+    
+            case 2 :
+                
+            break;
+
+            case 3 :
+
+            break;
+    
+        }
+        
+    }
+
+
+
+    //  ########################################################
+    //  #                    이벤트 핸들러                     #
+    //  ########################################################
+
+    
+
+    const numberItem      = document.createElement('div');
+    const numberText      = document.createElement('span');
+    const buttonDelete    = document.createElement('button');
+
+    // 생성 버튼
+    sectionSet[2].objects.generateElement3_button.addEventListener('click', () => {
+
+        numberText.textContent   = getLottoNumber();
+
+        sectionSet[2].objects.generateElement4_list.appendChild(numberItem);
+        sectionSet[2].objects.generateElement4_list.appendChild(numberText);
+
+        createDelete();
+
+    });
+
+    const createDelete = function() {
+
+        buttonDelete.textContent = '삭제';
+        sectionSet[2].objects.generateElement4_list.appendChild(buttonDelete);
+
+    };
+
+    const scrollLoop = function() {
+
+        playScroll();
+
+    }
+
+    // 스크롤 반응
+    window.addEventListener('scroll', () => {
+
+        scrollLoop();
+
+        currentSection    = getCurrentSection();
+        prevSectionHeight = getPrevSectionHeight();
+
+        yOffset           = window.scrollY + (window.innerHeight / 2);
+        sectionYOffset    = yOffset - prevSectionHeight;
+
+    });
+
+
+
+    //  ########################################################
+    //  #                        호 출                         #
+    //  ########################################################
+
+    window.addEventListener('load', () => {
+
+        initHTMLPage();
+
+    });
+
+})();
---
+++ readme.txt
@@ -0,0 +1,152 @@
+2022년 05월 27일
+- main.css : html
+  스크롤링 효과 변경, 마우스 커서 링크 접촉 시, 텍스트 입력 아이콘으로 출력되는 버그 수정 (EventHandler → scroll-behavior)
+- index.html : background-video
+  상세 : 배경영상의 속도를 줄여 시각적 안정성을 확보
+         playbackRate를 낯출 시, 30fps 이하의 저 프레임 원본소스는 끊김현상으로 시각적 불편함 발생
+         인코딩 문제 해결중 (SVP : SmoothVideo Project - 유료, 기술적 문제)
+
+
+2022년 05월 26일
+- index.html : background-video
+  밝은 영상으로 대체
+- main.css : html (text-shadow)
+  폰트 외곽선 효과 적용
+
+
+2022년 05월 23일
+- main.css : button-Generate
+  버튼 디자인 적용 (Source : https://copy-paste-css.com - forem)
+- main.css : section-2
+  1016회 당첨 번호 업데이트
+  번호 색채 적용 (Source : https://dhlottery.co.kr/gameResult.do?method=byWin)
+
+
+2022년 05월 22일
+- main.js
+  스크롤형 애니메이션 적용 중 (섹션 별 비율 값 조정 중)
+
+
+2022년 05월 21일
+- index.html : section, div, h1, h2
+- main.css : section, div, h1, h2
+  id와 class 정리, 가독성 향상
+- main.css : global-nav (z-index)
+  메뉴바의 블러 이펙트가 인트로 텍스트에 적용되지 않는 버그 수정
+- main.css : global-nav
+  메뉴바 상부 간격 추가
+- main.js : loadCanvasImages
+  main.js : drawCanvas
+  CanvasContext에 페이드 아웃 애니메이션 적용 실패로 영상형 배경화면으로 변경
+
+
+2022년 05월 20일
+- index.html : background-canvas
+- main.js : loadCanvasImages
+  main.js : drawCanvas
+  배경 캔버스 스크롤형 애니메이션 효과
+- index.html : iframe
+  iframe의 보안 문제로 인해 Vanilla Javascript를 통한 공식 사이트와의 데이터 연계 문제점 확인
+  임시로 매주 수동 업데이트하도록 조치
+
+
+2022년 05월 19일
+- index.html : section-0 (class message-1 → id intro-text1)
+  index.html : section-0 (class message-2 → id intro-text2)
+  index.html : section-0 (class message)
+  main.js : objects
+  Opacity 적용 위치 변경. 페이지 시작 시, 텍스트 Stutting 버그 수정
+
+
+2022년 05월 18일
+- index.html : html
+  (IBM Plex Sans KR 500 → SF-Pro-Rounded-Black)
+  폰트 업데이트
+
+
+2022년 05월 17일
+
+- index.html : video
+  영상 삽입
+  
+- main.css : background-video
+  영상을 페이지 배경으로 고정
+  블러 효과 설정
+
+- main.css : background-color
+  (background-color : white → black)
+  페이지 흰색 테두리 제거를 위해 배경화면 색상 변경
+
+- index.html : global-nav-logo
+  (photoshop text color : black → white)
+  배경색에 맞춘 로고 텍스트 색상 변경
+
+
+2022년 05월 16일
+
+- 코드 정리 분류 & 주석 추가
+
+
+2022년 05월 15일
+
+- global-nav
+  메뉴바에 블러 효과 추가
+
+- index.html : head
+  (IBM Plex Sans KR 500)
+  폰트 변경
+
+
+2022년 05월 14일
+
+- main.js : playIntro
+  main.js : introFadeOut
+  인트로 애니메이션 추가 (Interval형 Fade-Out효과)
+
+
+2022년 05월 13일
+
+- index.html : section-2
+  main.js : getLottoNumber
+  main.js : inputGenNum.addEventListener
+  기본적인 번호 생성 기능을 추가
+
+- 코드 정리 분류 & 주석 추가
+
+
+2022년 05월 11일
+
+- main.css : section-0
+  (flex-direction : default → column)
+  크기 변화에 따른 유동적인 수평 조절 업데이트
+  
+- main.css : global-nav-item
+  (margin-right : em → %)
+  메뉴 간격 단위를 변경
+
+
+2022년 05월 09일
+
+- main.css : global-nav
+  메뉴바를 고정하여 스크롤에도 이동하지 않도록 적용
+
+- index.html : section-0 ~ section-3
+  main.js : sectionSet
+  스크롤 반응형 애니메이션 제작을 위해 목적별 섹션을 분류
+
+- index.html : section-0
+  인트로 문구 삽입
+
+
+2022년 05월 07일
+
+- index.html : global-nav-logo
+  메뉴 좌측에 로고 삽입
+
+- main.css : global-nav-item
+  메뉴 링크 우측으로 정렬
+
+2022년 05월 06일
+- 프로젝트 시작
+- 초기 계획안 샘플
+  Kakao Oven (https://ovenapp.io/project/DBr1y8DhfN2krCxEEoxoFDqvk70yptr2)
